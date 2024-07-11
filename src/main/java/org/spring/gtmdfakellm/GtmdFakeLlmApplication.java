f819fc7caef8c58ede446c869e636ca911b44495
nbYDY <nbydy@tju.edu.cn>
2024-07-12 01:21:30 +0800

    完成了初期前端页面

---
+++ src/main/java/org/spring/gtmdfakellm/GtmdFakeLlmApplication.java
@@ -0,0 +1,12 @@
+package org.spring.gtmdfakellm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtmdFakeLlmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GtmdFakeLlmApplication.class, args);
+    }
+}
---
+++ src/test/java/org/spring/gtmdfakellm/GtmdFakeLlmApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.gtmdfakellm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtmdFakeLlmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
